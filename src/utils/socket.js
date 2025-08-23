c482fba4b5aa3cea5288ec7762253fb677204cc1
Preyanshu <preyanshudhapola6184@gmail.com>
2025-08-24 00:14:09 +0530

    path config done

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,10 @@ import { io } from "socket.io-client"
 import { BASE_URL } from "./constant"
 
 export const createSocketConnection=()=>{
-    return io(BASE_URL) // Telling client connect to backend system
+    if(location.hostname==="localjost"){
+        return io(BASE_URL);
+    }else{
+        return io("/",{path: "/api/socket.io"}) // Telling client connect to backend system
+    }
+    
 }
